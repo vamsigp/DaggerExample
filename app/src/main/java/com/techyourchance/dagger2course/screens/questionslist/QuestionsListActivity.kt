7a35e619649e3258baaf73f1039844d65f857337
Vamsi <vkrishna84@gmail.com>
2021-06-03 16:16:08 +0530

    Initial App checkin
    Initial App checkin

---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListActivity.kt
@@ -0,0 +1,88 @@
+package com.techyourchance.dagger2course.screens.questionslist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.techyourchance.dagger2course.Constants
+import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.questions.Question
+import com.techyourchance.dagger2course.screens.common.dialogs.ServerErrorDialogFragment
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    private lateinit var stackoverflowApi: StackoverflowApi
+
+    private var isDataLoaded = false
+
+    private lateinit var viewMvc: QuestionsListViewMvc
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewMvc = QuestionsListViewMvc(LayoutInflater.from(this), null)
+
+        setContentView(viewMvc.rootView)
+
+        // init retrofit
+        val retrofit = Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (!isDataLoaded) {
+            fetchQuestions()
+        }
+        viewMvc.registerListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        coroutineScope.coroutineContext.cancelChildren()
+        viewMvc.unregisterListener(this)
+    }
+
+    private fun fetchQuestions() {
+        coroutineScope.launch {
+            viewMvc.showProgressIndication()
+            try {
+                val response = stackoverflowApi.lastActiveQuestions(20)
+                if (response.isSuccessful && response.body() != null) {
+                    viewMvc.bindQuestions(response.body()!!.questions)
+                    isDataLoaded = true
+                } else {
+                    onFetchFailed()
+                }
+            } catch (t: Throwable) {
+                if (t !is CancellationException) {
+                    onFetchFailed()
+                }
+            } finally {
+                viewMvc.hideProgressIndication()
+            }
+        }
+    }
+
+    private fun onFetchFailed() {
+        supportFragmentManager.beginTransaction()
+                .add(ServerErrorDialogFragment.newInstance(), null)
+                .commitAllowingStateLoss()
+    }
+
+    override fun onRefreshClicked() {
+        fetchQuestions()
+    }
+
+    override fun onQuestionClick(clickedQuestion: Question) {
+        QuestionDetailsActivity.start(this, clickedQuestion.id)
+    }
+}
