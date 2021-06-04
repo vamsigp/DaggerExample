7236ec6ad4673be2deda604c8042e488b5400362
Vamsi <vkrishna84@gmail.com>
2021-06-04 13:34:22 +0530

    initialize network components lazy
    initialize network components lazy

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/BaseActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/BaseActivity.kt
@@ -2,7 +2,6 @@ package com.techyourchance.dagger2course.screens.common
 
 import androidx.appcompat.app.AppCompatActivity
 import com.techyourchance.dagger2course.MyApplication
-import com.techyourchance.dagger2course.screens.common.composition.AppCompositionRoot
 
 open class BaseActivity : AppCompatActivity() {
 
