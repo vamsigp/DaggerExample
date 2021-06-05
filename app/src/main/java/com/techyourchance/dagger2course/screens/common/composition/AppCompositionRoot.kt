c63b09e1d3566a8b4a9f33eabdf0fa6757e0aca9
Vamsi <vkrishna84@gmail.com>
2021-06-05 12:14:27 +0530

    initialize network componenet lazily
    initialize network componenet lazily

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/composition/AppCompositionRoot.kt
@@ -1,19 +1,25 @@
 package com.techyourchance.dagger2course.screens.common.composition
 
+import androidx.annotation.UiThread
 import com.techyourchance.dagger2course.Constants
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackoverflowApi: StackoverflowApi by lazy {
+        retrofit.create(StackoverflowApi::class.java)
+    }
 
     val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
 }
